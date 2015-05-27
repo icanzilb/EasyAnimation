1c749237ef9d0447e0b6762397bcf6fdebd23b9c
Marin Todorov <marin@underplot.com>
2015-05-27 18:03:31 +0200

    * push empty to cocoa pods

---
+++ EasyAnimation/EAAnimationDelayed.swift
@@ -0,0 +1,17 @@
+//
+//  EAAnimationDelayed.swift
+//  DemoApp
+//
+//  Created by Marin Todorov on 5/26/15.
+//  Copyright (c) 2015 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+
+class EAAnimationDelayed: Equatable {
+}
+
+func == (lhs: EAAnimationDelayed , rhs: EAAnimationDelayed) -> Bool {
+    return lhs === rhs
+}
+
