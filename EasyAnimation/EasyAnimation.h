26ccd1ef31b865cd11b0ffd229e465f2183b4afa
Ian Ynda-Hummel <ianynda@gmail.com>
2015-08-26 10:19:47 -0400

    Add an xcodeproj for Carthage support

---
+++ EasyAnimation/EasyAnimation.h
@@ -0,0 +1,17 @@
+//
+//  EasyAnimation.h
+//  EasyAnimation
+//
+//  Created by Ian Ynda-Hummel on 8/26/15.
+//  Copyright (c) 2015 EasyAnimation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyAnimation.
+FOUNDATION_EXPORT double EasyAnimationVersionNumber;
+
+//! Project version string for EasyAnimation.
+FOUNDATION_EXPORT const unsigned char EasyAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyAnimation/PublicHeader.h>
